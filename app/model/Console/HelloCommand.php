0714f06af4dea4d2b1b1bd0850c8e60c00390c59
Milan Felix Šulc <rkfelix@gmail.com>
2019-01-25 16:56:22 +0100

    Feature: added contributte/console-extra

---
+++ app/model/Console/HelloCommand.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace App\Model\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloCommand extends Command
+{
+
+	protected function configure(): void
+	{
+		$this->setName('hello');
+		$this->setDescription('Hello world!');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): void
+	{
+		$output->write('Hello world!');
+	}
+
+}
