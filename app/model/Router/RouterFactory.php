0af149715673ecd3c5ee73ffe2de423869a7a8c3
Petr Parolek <petr.parolek@gmail.com>
2021-12-01 21:08:57 +0100

    Moved example of using contributte/pdf from contributte/playground (#265)
    * fixed ignoring temp files
    
    * moved example of using contributte/pdf from contributte/playground

--- app/model/Router/RouterFactory.php
+++ app/model/Router/RouterFactory.php
@@ -13,6 +13,7 @@ final class RouterFactory
 		$router = new RouteList();
 
 		$this->buildMailing($router);
+		$this->buildPdf($router);
 		$this->buildAdmin($router);
 		$this->buildFront($router);
 
@@ -39,6 +40,14 @@ final class RouterFactory
 	{
 		$router[] = $list = new RouteList('Mailing');
 		$list[] = new Route('mailing/<presenter>/<action>[/<id>]', 'Home:default');
+
+		return $router;
+	}
+
+	protected function buildPdf(RouteList $router): RouteList
+	{
+		$router[] = $list = new RouteList('Pdf');
+		$list[] = new Route('pdf/<presenter>/<action>[/<id>]', 'Home:default');
 
 		return $router;
 	}
