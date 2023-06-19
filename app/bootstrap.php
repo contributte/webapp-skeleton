8bbf94c820eaee9578cae2ad6c79eb5f21ecbd26
Milan Felix Šulc <rkfelix@gmail.com>
2023-06-19 15:47:50 +0200

    Refactor: introduce Bootstrap as class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,40 +1,66 @@
 <?php declare(strict_types = 1);
 
+namespace App;
+
 use Contributte\Bootstrap\ExtraConfigurator;
+use Nette\Application\Application as NetteApplication;
 use Nette\DI\Compiler;
+use Symfony\Component\Console\Application as SymfonyApplication;
 use Tracy\Debugger;
 
-require __DIR__ . '/../vendor/autoload.php';
+final class Bootstrap
+{
 
-$configurator = new ExtraConfigurator();
-$configurator->setTempDirectory(__DIR__ . '/../var/tmp');
+	public static function boot(): ExtraConfigurator
+	{
+		$configurator = new ExtraConfigurator();
+		$configurator->setTempDirectory(__DIR__ . '/../var/tmp');
 
-$configurator->onCompile[] = function (ExtraConfigurator $configurator, Compiler $compiler): void {
-	// Add env variables to config structure
-	$compiler->addConfig(['parameters' => $configurator->getEnvironmentParameters()]);
-};
+		$configurator->onCompile[] = function (ExtraConfigurator $configurator, Compiler $compiler): void {
+			// Add env variables to config structure
+			$compiler->addConfig(['parameters' => $configurator->getEnvironmentParameters()]);
+		};
 
-// According to NETTE_DEBUG env
-$configurator->setEnvDebugMode();
+		// According to NETTE_DEBUG env
+		$configurator->setEnvDebugMode();
 
-// Enable tracy and configure it
-$configurator->enableTracy(__DIR__ . '/../var/log');
-Debugger::$errorTemplate = __DIR__ . '/resources/tracy/500.phtml';
+		// Enable tracy and configure it
+		$configurator->enableTracy(__DIR__ . '/../var/log');
+		Debugger::$errorTemplate = __DIR__ . '/resources/tracy/500.phtml';
 
-// Provide some parameters
-$configurator->addParameters([
-	'rootDir' => realpath(__DIR__ . '/..'),
-	'appDir' => __DIR__,
-	'wwwDir' => realpath(__DIR__ . '/../www'),
-]);
+		// Provide some parameters
+		$configurator->addParameters([
+			'rootDir' => realpath(__DIR__ . '/..'),
+			'appDir' => __DIR__,
+			'wwwDir' => realpath(__DIR__ . '/../www'),
+		]);
 
-// Load development or production config
-if (getenv('NETTE_ENV', true) === 'dev') {
-	$configurator->addConfig(__DIR__ . '/../config/env/dev.neon');
-} else {
-	$configurator->addConfig(__DIR__ . '/../config/env/prod.neon');
+		// Load development or production config
+		if (getenv('NETTE_ENV', true) === 'dev') {
+			$configurator->addConfig(__DIR__ . '/../config/env/dev.neon');
+		} else {
+			$configurator->addConfig(__DIR__ . '/../config/env/prod.neon');
+		}
+
+		$configurator->addConfig(__DIR__ . '/../config/local.neon');
+
+		return $configurator;
+	}
+
+	public static function runWeb(): void
+	{
+		self::boot()
+			->createContainer()
+			->getByType(NetteApplication::class)
+			->run();
+	}
+
+	public static function runCli(): void
+	{
+		self::boot()
+			->createContainer()
+			->getByType(SymfonyApplication::class)
+			->run();
+	}
+
 }
-
-$configurator->addConfig(__DIR__ . '/../config/local.neon');
-
-return $configurator->createContainer();
--- www/index.php
+++ www/index.php
@@ -1,10 +1,5 @@
 <?php declare(strict_types = 1);
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-//require '.maintenance.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Let bootstrap create Dependency Injection container.
-$container = require_once __DIR__ . '/../app/bootstrap.php';
-
-// Run application.
-$container->getByType(Nette\Application\Application::class)->run();
+App\Bootstrap::runWeb();
