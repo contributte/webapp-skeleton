14e00504b74d32e53ffc5b0b6bfd861603328e05
Milan Felix Šulc <rkfelix@gmail.com>
2023-10-30 15:54:24 +0100

    BaseForm: addFloat is in nette/forms

--- app/UI/Form/BaseForm.php
+++ app/UI/Form/BaseForm.php
@@ -8,16 +8,6 @@ use Nette\Forms\Controls\TextInput;
 class BaseForm extends Form
 {
 
-	public function addFloat(string $name, ?string $label = null): TextInput
-	{
-		$input = self::addText($name, $label);
-		$input->addCondition(self::FILLED)
-			->addRule(self::MAX_LENGTH, null, 255)
-			->addRule(self::FLOAT);
-
-		return $input;
-	}
-
 	public function addNumeric(string $name, ?string $label = null): TextInput
 	{
 		$input = self::addText($name, $label);
