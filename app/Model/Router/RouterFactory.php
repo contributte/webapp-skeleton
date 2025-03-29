77085420d7c1bdcdf376366e3d9403f6a60de054
Milan Felix Šulc <rkfelix@gmail.com>
2025-03-29 10:06:19 +0100

    WIP

--- app/Model/Router/RouterFactory.php
+++ app/Model/Router/RouterFactory.php
@@ -16,8 +16,6 @@ final class RouterFactory
 
 	public function create(): RouteList
 	{
-		$this->buildMailing();
-		$this->buildPdf();
 		$this->buildAdmin();
 		$this->buildFront();
 
@@ -34,18 +32,6 @@ final class RouterFactory
 	{
 		$this->router[] = $list = new RouteList('Front');
 		$list->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
-	}
-
-	protected function buildMailing(): void
-	{
-		$this->router[] = $list = new RouteList('Mailing');
-		$list->addRoute('mailing/<presenter>/<action>[/<id>]', 'Home:default');
-	}
-
-	protected function buildPdf(): void
-	{
-		$this->router[] = $list = new RouteList('Pdf');
-		$list->addRoute('pdf/<presenter>/<action>[/<id>]', 'Home:default');
 	}
 
 }
